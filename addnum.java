37f33a27ade9389cfe6fe780040427bbfe26d1cf
rrdadam <59324217+rrdadam@users.noreply.github.com>
2019-12-29 16:53:22 -0500

    Update addnum.java

--- addnum.java
+++ addnum.java
@@ -2,9 +2,9 @@ class addnum {
 
    public static void main(String[] args) {
         
-      int num , num1 , sum;
-      sum = num + num1;
+      int num , num1 , sub;
+      sub = num + num1;
 
-      System.out.println("Sum of these numbers: "+sum);
+      System.out.println("Sum of these numbers: "+sub);
    }
 }
